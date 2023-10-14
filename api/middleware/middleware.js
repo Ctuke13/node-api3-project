3aa432988b721c1517a135a5112642569e96c7f2
Chidi Tuke <c2kproductions@gmail.com>
2023-10-14 13:02:17 -0400

    wip

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,9 +1,24 @@
+const Users = require("../users/users-model.js");
+
 function logger(req, res, next) {
   // DO YOUR MAGIC
+  console.log(`[${new Date().toISOString()}] ${req.method} to ${req.url}`);
+  next();
 }
 
-function validateUserId(req, res, next) {
+async function validateUserId(req, res, next) {
   // DO YOUR MAGIC
+  try {
+    const user = await Users.getById(req.params.id);
+    if (user) {
+      req.user = user;
+      next();
+    } else {
+      next({ status: 404, message: `Hub ${req.params.id} not found` });
+    }
+  } catch (err) {
+    next(err);
+  }
 }
 
 function validateUser(req, res, next) {
@@ -15,3 +30,9 @@ function validatePost(req, res, next) {
 }
 
 // do not forget to expose these functions to other modules
+module.exports = {
+  logger,
+  validateUserId,
+  validateUser,
+  validatePost,
+};
